6487694588ce733327335c7cb8395a0977177471
kevinpro <43642508+Ricardokevins@users.noreply.github.com>
2021-09-29 00:19:34 +0800

    update

---
+++ VAEGenerator/TransformVAE.py
@@ -0,0 +1,291 @@
+# code by Tae Hwan Jung(Jeff Jung) @graykode, Derek Miller @dmmiller612
+# Reference : https://github.com/jadore801120/attention-is-all-you-need-pytorch
+#           https://github.com/JayParks/transformer
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from transformers import BertTokenizer
+pretrain_path = '../../../PTM//bert-base-chinese'
+bert_tokenizer = BertTokenizer.from_pretrained(pretrain_path)
+dict_size = len(bert_tokenizer.vocab)
+src_vocab_size = dict_size
+tgt_vocab_size = dict_size
+src_len = 5 # length of source
+tgt_len = 5 # length of target
+
+d_model = 512  # Embedding Size
+d_ff = 2048  # FeedForward dimension
+d_k = d_v = 64  # dimension of K(=Q), V
+n_layers = 6  # number of Encoder of Decoder Layer
+n_heads = 8  # number of heads in Multi-Head Attention
+
+# S: Symbol that shows starting of decoding input
+# E: Symbol that shows starting of decoding output
+# P: Symbol that will fill in blank sequence if current batch data size is short than time steps
+
+class PositionalEncoding(nn.Module):
+    "Implement the PE function."
+    def __init__(self, d_model = 512, dropout = 0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) *
+                             -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + Variable(self.pe[:, :x.size(1)], 
+                         requires_grad=False)
+        return self.dropout(x)
+
+
+def get_sinusoid_encoding_table(n_position, d_model):
+    def cal_angle(position, hid_idx):
+        return position / np.power(10000, 2 * (hid_idx // 2) / d_model)
+    def get_posi_angle_vec(position):
+        return [cal_angle(position, hid_j) for hid_j in range(d_model)]
+
+    sinusoid_table = np.array([get_posi_angle_vec(pos_i) for pos_i in range(n_position)])
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
+    return torch.FloatTensor(sinusoid_table)
+
+def get_attn_pad_mask(seq_q, seq_k):
+    # print(seq_q)
+    batch_size, len_q = seq_q.size()
+    batch_size, len_k = seq_k.size()
+    # eq(zero) is PAD token
+    pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)  # batch_size x 1 x len_k(=len_q), one is masking
+    return pad_attn_mask.expand(batch_size, len_q, len_k)  # batch_size x len_q x len_k
+
+def get_attn_subsequent_mask(seq):
+    attn_shape = [seq.size(0), seq.size(1), seq.size(1)]
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1)
+    subsequent_mask = torch.from_numpy(subsequent_mask).byte()
+    return subsequent_mask
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self):
+        super(ScaledDotProductAttention, self).__init__()
+
+    def forward(self, Q, K, V, attn_mask):
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(d_k) # scores : [batch_size x n_heads x len_q(=len_k) x len_k(=len_q)]
+        scores.masked_fill_(attn_mask, -1e9) # Fills elements of self tensor with value where mask is one.
+        attn = nn.Softmax(dim=-1)(scores)
+        context = torch.matmul(attn, V)
+        return context, attn
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self):
+        super(MultiHeadAttention, self).__init__()
+        self.W_Q = nn.Linear(d_model, d_k * n_heads)
+        self.W_K = nn.Linear(d_model, d_k * n_heads)
+        self.W_V = nn.Linear(d_model, d_v * n_heads)
+        self.linear = nn.Linear(n_heads * d_v, d_model)
+        self.layer_norm = nn.LayerNorm(d_model)
+
+    def forward(self, Q, K, V, attn_mask):
+        # q: [batch_size x len_q x d_model], k: [batch_size x len_k x d_model], v: [batch_size x len_k x d_model]
+        residual, batch_size = Q, Q.size(0)
+        # (B, S, D) -proj-> (B, S, D) -split-> (B, S, H, W) -trans-> (B, H, S, W)
+        q_s = self.W_Q(Q).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # q_s: [batch_size x n_heads x len_q x d_k]
+        k_s = self.W_K(K).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # k_s: [batch_size x n_heads x len_k x d_k]
+        v_s = self.W_V(V).view(batch_size, -1, n_heads, d_v).transpose(1,2)  # v_s: [batch_size x n_heads x len_k x d_v]
+
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_heads, 1, 1) # attn_mask : [batch_size x n_heads x len_q x len_k]
+
+        # context: [batch_size x n_heads x len_q x d_v], attn: [batch_size x n_heads x len_q(=len_k) x len_k(=len_q)]
+        context, attn = ScaledDotProductAttention()(q_s, k_s, v_s, attn_mask)
+        context = context.transpose(1, 2).contiguous().view(batch_size, -1, n_heads * d_v) # context: [batch_size x len_q x n_heads * d_v]
+        output = self.linear(context)
+        return self.layer_norm(output + residual), attn # output: [batch_size x len_q x d_model]
+
+class PoswiseFeedForwardNet(nn.Module):
+    def __init__(self):
+        super(PoswiseFeedForwardNet, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
+        self.layer_norm = nn.LayerNorm(d_model)
+
+    def forward(self, inputs):
+        residual = inputs # inputs : [batch_size, len_q, d_model]
+        output = nn.ReLU()(self.conv1(inputs.transpose(1, 2)))
+        output = self.conv2(output).transpose(1, 2)
+        return self.layer_norm(output + residual)
+
+class EncoderLayer(nn.Module):
+    def __init__(self):
+        super(EncoderLayer, self).__init__()
+        self.enc_self_attn = MultiHeadAttention()
+        self.pos_ffn = PoswiseFeedForwardNet()
+
+    def forward(self, enc_inputs, enc_self_attn_mask):
+        enc_outputs, attn = self.enc_self_attn(enc_inputs, enc_inputs, enc_inputs, enc_self_attn_mask) # enc_inputs to same Q,K,V
+        enc_outputs = self.pos_ffn(enc_outputs) # enc_outputs: [batch_size x len_q x d_model]
+        return enc_outputs, attn
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.src_emb = nn.Embedding(src_vocab_size, d_model)
+        self.pos_emb = PositionalEncoding()
+        #self.pos_emb = nn.Embedding.from_pretrained(get_sinusoid_encoding_table(src_len+1, d_model),freeze=True)
+        self.layers = nn.ModuleList([EncoderLayer() for _ in range(n_layers)])
+
+    def forward(self, enc_inputs): # enc_inputs : [batch_size x source_len]
+        #enc_outputs = self.src_emb(enc_inputs) + self.pos_emb(torch.LongTensor([[1,2,3,4,0]]))
+        enc_outputs = self.pos_emb(self.src_emb(enc_inputs))
+        enc_self_attn_mask = get_attn_pad_mask(enc_inputs, enc_inputs)
+        enc_self_attns = []
+        for layer in self.layers:
+            enc_outputs, enc_self_attn = layer(enc_outputs, enc_self_attn_mask)
+            enc_self_attns.append(enc_self_attn)
+        return enc_outputs, enc_self_attns
+
+class DecoderLayer(nn.Module):
+    def __init__(self):
+        super(DecoderLayer, self).__init__()
+        self.dec_self_attn = MultiHeadAttention()
+        self.dec_enc_attn = MultiHeadAttention()
+        self.pos_ffn = PoswiseFeedForwardNet()
+
+    def forward(self, dec_inputs, enc_outputs, dec_self_attn_mask, dec_enc_attn_mask):
+        dec_outputs, dec_self_attn = self.dec_self_attn(dec_inputs, dec_inputs, dec_inputs, dec_self_attn_mask)
+        dec_outputs, dec_enc_attn = self.dec_enc_attn(dec_outputs, enc_outputs, enc_outputs, dec_enc_attn_mask)
+        dec_outputs = self.pos_ffn(dec_outputs)
+        return dec_outputs, dec_self_attn, dec_enc_attn
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.tgt_emb = nn.Embedding(tgt_vocab_size, d_model)
+        self.pos_emb = PositionalEncoding()
+        #self.pos_emb = nn.Embedding.from_pretrained(get_sinusoid_encoding_table(tgt_len+1, d_model),freeze=True)
+        self.layers = nn.ModuleList([DecoderLayer() for _ in range(n_layers)])
+
+    def forward(self, dec_inputs, enc_inputs, enc_outputs): # dec_inputs : [batch_size x target_len]
+        #dec_outputs = self.tgt_emb(dec_inputs) + self.pos_emb(torch.LongTensor([[5,1,2,3,4]]))
+        dec_outputs = self.pos_emb(self.tgt_emb(dec_inputs))
+        dec_self_attn_pad_mask = get_attn_pad_mask(dec_inputs, dec_inputs)
+        #print(dec_self_attn_pad_mask)
+        dec_self_attn_subsequent_mask = get_attn_subsequent_mask(dec_inputs)
+        #print(dec_self_attn_subsequent_mask)
+        
+        dec_self_attn_mask = torch.gt((dec_self_attn_pad_mask + dec_self_attn_subsequent_mask), 0)
+        #print(dec_self_attn_mask)
+        #exit()
+        dec_enc_attn_mask = get_attn_pad_mask(dec_inputs, enc_inputs)
+        dec_self_attns, dec_enc_attns = [], []
+        for layer in self.layers:
+            dec_outputs, dec_self_attn, dec_enc_attn = layer(dec_outputs, enc_outputs, dec_self_attn_mask, dec_enc_attn_mask)
+            dec_self_attns.append(dec_self_attn)
+            dec_enc_attns.append(dec_enc_attn)
+        return dec_outputs, dec_self_attns, dec_enc_attns
+
+class TransformerVAE(nn.Module):
+    def __init__(self):
+        super(Transformer, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+        self.projection = nn.Linear(d_model, tgt_vocab_size, bias=False)
+        self.fc1 = nn.Linear(d_model, d_model)  # mu
+        self.fc2 = nn.Linear(d_model, h_dim)  # log_var
+        self.fc3 = nn.Linear(d_model, h_dim)
+        # 解码器 ： [b, z_dim] => [b, input_dim]
+        self.fc4 = nn.Linear(h_dim, d_model)
+        self.fc5 = nn.Linear(d_model, self.output_dim)
+        
+    def forward(self, enc_inputs, dec_inputs):
+        enc_outputs, enc_self_attns = self.encoder(enc_inputs)
+        dec_outputs, dec_self_attns, dec_enc_attns = self.decoder(dec_inputs, enc_inputs, enc_outputs)
+        dec_logits = self.projection(dec_outputs) # dec_logits : [batch_size x src_vocab_size x tgt_vocab_size]
+        return dec_logits.view(-1, dec_logits.size(-1)), enc_self_attns, dec_self_attns, dec_enc_attns
+
+
+class VAE(nn.Module):
+    def __init__(self, input_dim=768, h_dim=400):
+        # 调用父类方法初始化模块的state
+        super(VAE, self).__init__()
+        self.output_dim = len(bert_tokenizer.vocab)
+        self.input_dim = input_dim
+        self.h_dim = h_dim
+        
+
+        # 编码器 ： [b, input_dim] => [b, z_dim]
+        
+        #self.scaler = nn.Linear(input_dim, input_dim//4) 
+        self.fc1 = nn.Linear(input_dim, input_dim)  # mu
+        self.fc2 = nn.Linear(input_dim, h_dim)  # log_var
+        self.fc3 = nn.Linear(input_dim, h_dim)
+        # 解码器 ： [b, z_dim] => [b, input_dim]
+        self.fc4 = nn.Linear(h_dim, input_dim)
+        self.fc5 = nn.Linear(input_dim, self.output_dim)
+
+    def forward(self, x):
+        """
+        向前传播部分, 在model_name(inputs)时自动调用
+        :param x: the input of our training model [b, batch_size, 1, 28, 28]
+        :return: the result of our training model
+        """
+        x = self.encoder(x)[0]
+
+        # encoder
+        mu, log_var = self.encode(x)
+        # reparameterization trick
+        sampled_z = self.reparameterization(mu, log_var)
+        #print(sampled_z.shape)
+        # decoder
+        x_hat = self.decode(sampled_z)
+        # reshape
+        #print(x_hat.shape)
+        #x_hat = x_hat.view(batch_size, 1, 28, 28)
+        return x_hat, mu, log_var
+
+    def encode(self, x):
+        """
+        encoding part
+        :param x: input image
+        :return: mu and log_var
+        """
+        #x = self.scaler(x)
+        #print(x.shape)
+        #batch_size = x.shape[0]
+        #x = x.reshape(batch_size,-1)
+        #print(x.shape)
+        h = F.relu(self.fc1(x))
+        # print(h.shape)
+        mu = self.fc2(h)
+        log_var = self.fc3(h)
+        # print(mu.shape)
+        # print(log_var.shape)
+        return mu, log_var
+
+    def reparameterization(self, mu, log_var):
+        """
+        Given a standard gaussian distribution epsilon ~ N(0,1),
+        we can sample the random variable z as per z = mu + sigma * epsilon
+        :param mu:
+        :param log_var:
+        :return: sampled z
+        """
+        sigma = torch.exp(log_var * 0.5)
+        eps = torch.randn_like(sigma)
+        return mu + sigma * eps  # 这里的“*”是点乘的意思
+
+    def decode(self, z):
+        """
+        Given a sampled z, decode it back to image
+        :param z:
+        :return:
+        """
+        h = F.relu(self.fc4(z))
+        x_hat = torch.sigmoid(self.fc5(h))  # 图片数值取值为[0,1]，不宜用ReLU
+        return x_hat
