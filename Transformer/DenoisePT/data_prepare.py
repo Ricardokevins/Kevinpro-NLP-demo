9d2172572aee250eee4bbec6794b8055fbbba735
kevinpro <3121416933@qq.com>
2022-01-28 01:05:52 +0800

    Update data_prepare.py

--- Transformer/DenoisePT/data_prepare.py
+++ Transformer/DenoisePT/data_prepare.py
@@ -1,9 +1,18 @@
 from EasyTransformer import file_utils as fop
 
+import random
+def apply_noise(sent):
+    # Random delete
+    # Random replace
+    # Replace with similar spell?
+    pass
+
 def preprocess():
     lines = fop.read_txt_lines('./data/cnews.train.txt')
     lines = [i.split('\t')[1] for i in lines]
     print(lines[0])
     fop.write_txt_file('./data/corpus.txt', lines)
+
+
 
 preprocess()
